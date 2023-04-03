65ad54d0d2d880dce7c93428da008dd7a22c5c60
TsalemisIasonas <tsalemisi@gmail.com>
2023-04-04 00:13:59 +0300

    update

--- energy_scraper.py
+++ energy_scraper.py
@@ -5,7 +5,9 @@ from bs4 import BeautifulSoup
 
 class EnergyScraper:
     def __init__(self):
+        self.energy_data = {}
         self.url = 'https://transparency.entsoe.eu/generation/r2/actualGenerationPerProductionType/show'
+        self.scrape_data()
 
     def fetch_soup(self):
         response = requests.get(self.url)
@@ -15,16 +17,36 @@ class EnergyScraper:
     def scrape_data(self):
         self.headers = []
         soup = self.fetch_soup()
-        table = soup.find("div",{"class":"table-container"})
-        headers = table.find_all('th',{'class':"dv-header-right full-border vertical-center"})
+        self.table = soup.find("div",{"class":"table-container"})
+
+    def get_headers(self):
+        headers = self.table.find_all('th',{'class':"dv-header-right full-border vertical-center"})
         for i in headers:
             self.headers.append(i.find('span').text.replace('\n',''))
         for i in range(len(self.headers)):
             self.headers[i] = self.headers[i].replace(' ','')
-            
-        return self.headers
+        # self.energy_data = {key: "" for key in self.headers}
+        # print(self.energy_data)
+    
+    def get_data(self):
+        temp = []
+        self.data = []
+        data = self.table.find('tbody')
+        rows = data.find_all('tr')
+        print(len(rows))
+        for row in rows:
+            containers = row.find_all('td')
+            temp.append(containers)
+            # for container in containers:
+            #     value = container.find('span')
+            #     temp.append(value)
+            for i in temp:
+                print(i)
 
 
-# if __name__ == '__main__':
-#     scraper = EnergyScraper()
-#     scraper.scrape_data()
+
+if __name__ == '__main__':
+    scraper = EnergyScraper()
+    # scraper.get_headers()
+    scraper.get_data()
+    
