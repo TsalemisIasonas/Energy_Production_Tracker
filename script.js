1cceba10d2c19af2de769fc2cd72fcabf1c31590
TsalemisIasonas <tsalemisi@gmail.com>
2023-04-28 14:48:15 +0300

    footer - transitions - canvas style

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-const themeToggleIcon = document.querySelector('#theme-toggle-button i');
-themeToggleIcon.addEventListener('click',themeToggler);
+const themeToggleIcon = document.querySelector('#theme-toggle-button i'),
+      footer = document.querySelector('#main-footer');
+themeToggleIcon.addEventListener('click', themeToggler);
 
 
 function themeToggler() {
-    if (themeToggleIcon.classList.contains('dark-theme-icon')) {
-      themeToggleIcon.classList.remove('dark-theme-icon');
-      themeToggleIcon.classList.add('light-theme-icon');
-      document.documentElement.style.setProperty('--bg-color', 'white');
-      document.documentElement.style.setProperty('--fg-color','black');
-    }
-    else {
-      themeToggleIcon.classList.remove('light-theme-icon');
-      themeToggleIcon.classList.add('dark-theme-icon');
-      document.documentElement.style.setProperty('--bg-color', '#252525');
-      document.documentElement.style.setProperty('--fg-color','white');
-    }
+  if (themeToggleIcon.classList.contains('dark-theme-icon')) {
+    themeToggleIcon.classList.remove('dark-theme-icon');
+    themeToggleIcon.classList.add('light-theme-icon');
+    document.documentElement.style.setProperty('--bg-color', 'white');
+    document.documentElement.style.setProperty('--fg-color', 'black');
   }
-  
-  window.onload = function() {
-    console.log('done');
+  else {
+    themeToggleIcon.classList.remove('light-theme-icon');
+    themeToggleIcon.classList.add('dark-theme-icon');
+    document.documentElement.style.setProperty('--bg-color', '#252525');
+    document.documentElement.style.setProperty('--fg-color', 'white');
   }
+}
+
+window.onload = function () {
+  console.log('done');
+}
+
+// show footer if user scrolls under all elements
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 700) {
+    footer.classList.add('footer-visible');
+    footer.classList.remove('footer-invisible');
+  } else {
+    footer.classList.remove('footer-visible');
+    footer.classList.add('footer-invisible');
+  }
+});
